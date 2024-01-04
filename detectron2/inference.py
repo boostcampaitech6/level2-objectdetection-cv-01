8f0c8e20b461faf0c015c49e18d1d36fa760e6a8
JasonPark <cjpark137@naver.com>
2024-01-05 04:24:47 +0900

    feat: train.py, inference.py 코드 추가

---
+++ detectron2/inference.py
@@ -0,0 +1,105 @@
+import os
+import copy
+import torch
+import argparse
+import pandas as pd
+
+from tqdm import tqdm
+
+from detectron2.data import detection_utils as utils
+from detectron2.utils.logger import setup_logger
+
+from detectron2.config import get_cfg
+from detectron2.engine import DefaultPredictor
+from detectron2.data.datasets import register_coco_instances
+from detectron2.data import build_detection_test_loader
+
+def register_dataset(args):
+    try:
+        register_coco_instances('coco_trash_test', {}, f'{args.data_dir}/test.json', args.data_dir)
+    except AssertionError:
+        pass
+
+def setup_config(args):
+    cfg = get_cfg()
+
+    cfg.merge_from_file(f'./configs/{args.config_path}/{args.model}.yaml')
+    
+    cfg.DATASETS.TEST = ('coco_trash_test',)
+
+    cfg.DATALOADER.NUM_WOREKRS = 2
+
+    save_path = os.path.join(args.output_dir, args.model)
+    os.makedirs(save_path, exist_ok = True)
+    cfg.OUTPUT_DIR = save_path
+
+    cfg.MODEL.WEIGHTS = os.path.join(cfg.OUTPUT_DIR, args.ckpt_name)
+
+    cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = 128
+    cfg.MODEL.ROI_HEADS.NUM_CLASSES = 10
+    cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.01
+
+    return cfg
+
+def TestMapper(dataset_dict):
+    
+    dataset_dict = copy.deepcopy(dataset_dict)
+    image = utils.read_image(dataset_dict['file_name'], format='BGR')
+    
+    dataset_dict['image'] = image
+    
+    return dataset_dict
+
+def inference(predictor):
+
+    test_loader = build_detection_test_loader(cfg, 'coco_trash_test', TestMapper)
+
+    prediction_strings = []
+    file_names = []
+
+    for data in tqdm(test_loader):
+        
+        prediction_string = ''
+        
+        data = data[0]
+        
+        outputs = predictor(data['image'])['instances']
+        
+        targets = outputs.pred_classes.cpu().tolist()
+        boxes = [i.cpu().detach().numpy() for i in outputs.pred_boxes]
+        scores = outputs.scores.cpu().tolist()
+        
+        for target, box, score in zip(targets,boxes,scores):
+            prediction_string += (str(target) + ' ' + str(score) + ' ' + str(box[0]) + ' ' 
+            + str(box[1]) + ' ' + str(box[2]) + ' ' + str(box[3]) + ' ')
+        
+        prediction_strings.append(prediction_string)
+        file_names.append(data['file_name'].replace(args.data_dir,''))
+
+    submission = pd.DataFrame()
+    submission['PredictionString'] = prediction_strings
+    submission['image_id'] = file_names
+    submission.to_csv(os.path.join(cfg.OUTPUT_DIR, f'submission.csv'), index=None)
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='Obejct Detection Inference by Detectron2')
+
+    parser.add_argument('--data_dir', type=str, default='/root/dataset/', help='data dir')
+    parser.add_argument('--output_dir', type=str, default='./output/')
+    parser.add_argument('--config_path', type=str, default='COCO-Detection', help='select config path')
+    parser.add_argument('--model', type=str, default='faster_rcnn_R_101_FPN_3x', help='train model name')
+    parser.add_argument('--ckpt_name', type=str, default='model_final.pth', help='load model .pth file name')
+    args = parser.parse_args()
+
+    return args
+
+if __name__ == '__main__':
+    args = parse_args()
+
+    register_dataset(args)
+
+    cfg = setup_config(args)
+
+    predictor = DefaultPredictor(cfg)
+
+    inference(predictor)
---
+++ detectron2/train.py
@@ -0,0 +1,143 @@
+import os
+import copy
+import json
+import torch
+import random
+import argparse
+import numpy as np
+
+from detectron2.data import detection_utils as utils
+from detectron2.utils.logger import setup_logger
+
+from detectron2 import model_zoo
+from detectron2.config import get_cfg
+from detectron2.engine import DefaultTrainer
+from detectron2.data import DatasetCatalog, MetadataCatalog
+from detectron2.data.datasets import register_coco_instances
+from detectron2.evaluation import COCOEvaluator
+from detectron2.data import build_detection_train_loader
+
+import detectron2.data.transforms as T
+
+def seed_everything(seed):
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)  
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+    np.random.seed(seed)
+    random.seed(seed)
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='Obejct Detection Train by Detectron2')
+
+    parser.add_argument('--seed', type=int, default=42, help='Fixed Seed')
+    parser.add_argument('--data_dir', type=str, default='/root/dataset/', help='Train data dir')
+    parser.add_argument('--output_dir', type=str, default='./output/')
+    parser.add_argument('--config_path', type=str, default='COCO-Detection', help='select config path')
+    parser.add_argument('--model', type=str, default='faster_rcnn_R_101_FPN_3x', help='train model name')
+    parser.add_argument('--epochs', type=int, default=1, help='train epochs')
+    args = parser.parse_args()
+
+    return args
+
+def setup_config(args):
+    cfg = get_cfg()
+
+    cfg.merge_from_file(f'./configs/{args.config_path}/{args.model}.yaml')
+    cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url(f'{args.config_path}/{args.model}.yaml')
+
+    cfg.DATASETS.TRAIN = ('coco_trash_train',)
+    cfg.DATASETS.TEST = ('coco_trash_test',)
+
+    cfg.DATALOADER.NUM_WOREKRS = 2
+
+    cfg.MODEL.MASK_ON = False
+    cfg.SOLVER.IMS_PER_BATCH = 16
+    
+    data = json.load(open(f'{args.data_dir}/train.json'))
+
+    cfg.SOLVER.MAX_ITER = int(len(data['images']) / cfg.SOLVER.IMS_PER_BATCH * args.epochs)
+    cfg.SOLVER.BASE_LR = 0.001
+    cfg.SOLVER.STEPS = (2000, 4000)
+    cfg.SOLVER.GAMMA = 0.005
+    cfg.SOLVER.CHECKPOINT_PERIOD = 3000
+
+    save_path = os.path.join(args.output_dir, args.model)
+    os.makedirs(save_path, exist_ok = True)
+    cfg.OUTPUT_DIR = save_path
+
+    cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = 128
+    cfg.MODEL.ROI_HEADS.NUM_CLASSES = 10
+
+    cfg.TEST.EVAL_PERIOD = 3000
+
+    return cfg
+
+def register_dataset(args):
+    for data in ['train', 'test']:
+        try:
+            register_coco_instances('coco_trash_' + data, {}, f'{args.data_dir}/{data}.json', args.data_dir)
+        except AssertionError:
+            pass
+        
+    MetadataCatalog.get("coco_trash_train").set(thing_classes=["General trash", "Paper", "Paper pack", "Metal", 
+                                                        "Glass", "Plastic", "Styrofoam", "Plastic bag", "Battery", "Clothing"])
+
+def TrainMapper(dataset_dict):
+    dataset_dict = copy.deepcopy(dataset_dict)
+    image = utils.read_image(dataset_dict['file_name'], format='BGR')
+    
+    transform_list = [
+        # T.Resize((1024, 1024)),
+        T.RandomFlip(prob=0.5, horizontal=True, vertical=False),
+        T.RandomBrightness(0.8, 1.8),
+        T.RandomContrast(0.6, 1.3),
+        T.RandomRotation(angle=0.5),
+        T.RandomLighting(scale=0.5),
+    ]
+    
+    image, transforms = T.apply_transform_gens(transform_list, image)
+    
+    dataset_dict['image'] = torch.as_tensor(image.transpose(2,0,1).astype('float32'))
+    
+    annos = [
+        utils.transform_instance_annotations(obj, transforms, image.shape[:2])
+        for obj in dataset_dict.pop('annotations')
+        if obj.get('iscrowd', 0) == 0
+    ]
+    
+    instances = utils.annotations_to_instances(annos, image.shape[:2])
+    dataset_dict['instances'] = utils.filter_empty_instances(instances)
+    
+    return dataset_dict
+
+class MyTrainer(DefaultTrainer):
+    
+    @classmethod
+    def build_train_loader(cls, cfg, sampler=None):
+        return build_detection_train_loader(
+        cfg, mapper = TrainMapper, sampler = sampler
+        )
+    
+    @classmethod
+    def build_evaluator(cls, cfg, dataset_name, output_folder=None):
+        if output_folder is None:
+            os.makedirs(cfg.OUTPUT_DIR, exist_ok = True)
+            output_folder = cfg.OUTPUT_DIR
+            
+        return COCOEvaluator(dataset_name, cfg, False, output_folder)
+
+if __name__ == '__main__':
+    args = parse_args()
+    seed_everything(args.seed)
+
+    register_dataset(args)
+    
+    cfg = setup_config(args)
+
+    trainer = MyTrainer(cfg)
+    trainer.resume_or_load(resume=False)
+    trainer.train()
+
+# logger = setup_logger()
