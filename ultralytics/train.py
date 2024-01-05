199526014b32b11967ad46a4a8314bb0972635c6
JasonPark <cjpark137@naver.com>
2024-01-06 05:05:38 +0900

    feat: add train and inference code

---
+++ ultralytics/train.py
@@ -0,0 +1,23 @@
+import argparse
+
+from ultralytics import YOLO
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("--cfg", type=str, default="custom.yaml", help="train config file path")
+    parser.add_argument("--weight", type=str, default="yolov8s.pt", help="yolov8 model weight(.pt) or path(outputs/~/best.pt)")
+
+    args = parser.parse_args()
+
+    return args
+
+def main():
+    args = parse_args()
+
+    model = YOLO(args.weight)
+
+    model.train(cfg=args.cfg, data='recycle.yaml')
+
+if __name__ == "__main__":
+    main()
