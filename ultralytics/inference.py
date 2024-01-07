9045202a345d27d4b3891a4e0933318369cb306a
JasonPark <cjpark137@naver.com>
2024-01-07 17:39:43 +0900

    fix: redefine train.py, inference.py

--- ultralytics/inference.py
+++ ultralytics/inference.py
@@ -1,6 +1,5 @@
 import argparse
 import os
-import os.path as osp
 
 import pandas as pd
 import torch
@@ -10,110 +9,80 @@ from tqdm import tqdm
 
 from ultralytics import YOLO
 
-
-def get_args():
+def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--model", type=str)
+    parser.add_argument("--model_path", type=str, help="Specify the file path of the model for training")
+    parser.add_argument("--data_path", type=str, default="/data/ephemeral/home/dataset_yolo/images", help="Set the file path where the training dataset images are stored")
+    parser.add_argument("--json_path", type=str, default="/data/ephemeral/home/dataset_yolo/test.json", help="Define the file path for the test JSON file")
     args = parser.parse_args()
     return args
-
 
 class CustomDataset(Dataset):
     def __init__(self, annotation, data_dir):
         super().__init__()
-        self.data_dir = data_dir  # data 경로 폴더
-        self.coco = COCO(annotation)  # coco annotation 불러오기 (coco API)
+        self.data_dir = data_dir
+        self.coco = COCO(annotation)
 
     def __getitem__(self, index: int):
         image_id = self.coco.getImgIds(imgIds=index)
         image_info = self.coco.loadImgs(image_id)[0]
-        image = osp.join(self.data_dir, image_info["file_name"])
+        image = os.path.join(self.data_dir, image_info["file_name"])
 
         return image
 
     def __len__(self) -> int:
         return len(self.coco.getImgIds())
 
-
-# 이미지의 예측된 box 얻어서 submission file 내용 만들기
 def get_bboxes(annotation, data_loader, model):
-    # submission 파일에 저장될 내용
     prediction_strings = []
     file_names = []
 
     COCO(annotation)
-    for batch_idx, image_batch in enumerate(tqdm(data_loader)):
-        # 이미지에서 예측한 모든 박스들
-
-        # 이미지 정보
+    for _, image_batch in enumerate(tqdm(data_loader)):
 
         with torch.no_grad():
             predictions = model(image_batch)
         batch_size = len(image_batch)
         for idx in range(batch_size):
-            file_name = osp.join("test", osp.basename(image_batch[idx]))
+            file_name = os.path.join("test", os.path.basename(image_batch[idx]))
             pred_info = predictions[idx].boxes
             classes = pred_info.cls.tolist()
             bboxes = pred_info.xyxy.tolist()
             conf = pred_info.conf.tolist()
 
-            # submission 파일에 저장될 내용
             prediction_string = ""
             for pred_class, conf, box in zip(classes, conf, bboxes):
-                prediction_string += (
-                    str(int(pred_class))
-                    + " "
-                    + str(conf)
-                    + " "
-                    + str(box[0])
-                    + " "
-                    + str(box[1])
-                    + " "
-                    + str(box[2])
-                    + " "
-                    + str(box[3])
-                    + " "
-                )
+                prediction_string += \
+                    (str(int(pred_class)) + " " + str(conf) + " " + 
+                     str(box[0]) + " " + str(box[1]) + " " + str(box[2]) + " " + str(box[3]) + " ")
+                
             prediction_strings.append(prediction_string)
             file_names.append(file_name)
 
     return prediction_strings, file_names
 
-
-from ultralytics import YOLO
-
-seed = 123
-torch.manual_seed(seed)
-
-args = get_args()
-
-# Hyperparameters etc.
-DEVICE = "cuda" if torch.cuda.is_available else "cpu"
-LOAD_MODEL_FILE = args.model  # inference에 사용할 model
-
-
-def inference():
-    # model 생성
-    model = YOLO(LOAD_MODEL_FILE)
-    # inference에 사용할 model 로드
+def inference(args):
+    model = YOLO(args.model)
 
     # annotation 경로
-    annotation = "/opt/ml/dataset_yolo/test.json"
-    data_dir = "/opt/ml/dataset_yolo/images"  # dataset 경로
+    annotation = args.json_path # ex. "/data/ephemeral/home/dataset_yolo/test.json"
+    data_dir = args.dataset_path # ex. "/data/ephemeral/home/dataset_yolo/images"
 
     # 데이터셋 로드
     test_dataset = CustomDataset(annotation, data_dir)
     test_data_loader = DataLoader(test_dataset, batch_size=8, shuffle=False, num_workers=4)
+
     # 예측 및 submission 파일 생성
     prediction_strings, file_names = get_bboxes(annotation, test_data_loader, model)
     submission = pd.DataFrame()
     submission["PredictionString"] = prediction_strings
     submission["image_id"] = file_names
     
-    path_lst = args.model.split('/')
-    save_path = os.path.join(*path_lst[:2])
-    submission.to_csv(os.path.join(save_path, f"./{path_lst[1]}_submission.csv"), index=None)
+    submission.to_csv("./yolo_submission.csv", index=None)
     print(submission.head())
 
 
-inference()
+if __name__ == "__main__":
+    args = parse_args()
+
+    inference(args)
--- ultralytics/train.py
+++ ultralytics/train.py
@@ -6,18 +6,19 @@ def parse_args():
     parser = argparse.ArgumentParser()
 
     parser.add_argument("--cfg", type=str, default="custom.yaml", help="train config file path")
-    parser.add_argument("--weight", type=str, default="yolov8s.pt", help="yolov8 model weight(.pt) or path(outputs/~/best.pt)")
+    parser.add_argument("--weight", type=str, default="yolov8x.pt", help="yolov8 model weight(.pt) or path(outputs/~/best.pt)")
 
     args = parser.parse_args()
 
     return args
 
-def main():
-    args = parse_args()
+def main(args):
 
     model = YOLO(args.weight)
 
     model.train(cfg=args.cfg, data='recycle.yaml')
 
 if __name__ == "__main__":
-    main()
+    args = parse_args()
+    
+    main(args)
