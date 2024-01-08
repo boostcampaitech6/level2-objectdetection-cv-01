d50819bc4d7e8fda6ec4bda89c2fcf1b8a1d46ce
Marlang <audgus4321@naver.com>
2024-01-08 12:15:41 +0900

    fix: update train.py

--- ultralytics/train.py
+++ ultralytics/train.py
@@ -1,8 +1,11 @@
 import argparse
 import yaml
+import wandb as wb
 
 from ultralytics import YOLO
 from ultralytics.utils import checks, yaml_load
+
+from ultralytics.engine import trainer
 
 def parse_args():
     parser = argparse.ArgumentParser()
@@ -12,6 +15,23 @@ def parse_args():
     args = parser.parse_args()
 
     return args
+
+def on_pretrain_routine_start(trainer) :
+    try :
+        with open(trainer.args.data, 'r') as file :
+            yaml_data = yaml.safe_load(file)
+            data_dir = yaml_data.get('path', None)
+    except Exception as e :
+        raise RuntimeError(f"Error reading YAML file: {e}") from e
+    
+    if not data_dir :
+        raise ValueError("Path not found in YAML file")
+    
+    wb.run or wb.init(project=trainer.args.project or 'YOLOv8',
+                      name=f'{trainer.args.model}_{trainer.args.batch}_{data_dir.split("/")[-2:]}',
+                      config=vars(trainer.args),
+                      entity='ai_tech_level2_objectdetection')
+
 
 def main(args):
 
@@ -25,3 +45,7 @@ if __name__ == "__main__":
     args = parse_args()
 
     main(args)
+
+
+
+
