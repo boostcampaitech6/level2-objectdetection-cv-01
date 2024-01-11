c7a1220ba46e19502dbedac8c42a3e4a228b8aa5
Jongwook Bae <bjonguk@gmail.com>
2024-01-12 01:01:07 +0900

    feat: super resolution (#26)
    * feat: generate super_resolution project
    
    #25
    
    * feat: remove super_resolution/EDSR-PyTorch/test folder
    
    #25
    
    * fix: remove __pycache__
    
    #25
    
    * #fix: remove src/__init__.py
    
    #25

---
+++ super_resolution/EDSR-PyTorch/src/main.py
@@ -0,0 +1,38 @@
+import torch
+
+import utility
+import data
+import model
+import loss
+from option import args
+from trainer import Trainer
+
+# GPU 디바이스 번호 설정
+device_id = 0
+torch.cuda.set_device(device_id)
+
+torch.manual_seed(args.seed)
+checkpoint = utility.checkpoint(args)
+
+def main():
+    global model
+    if args.data_test == ['video']:
+        from videotester import VideoTester
+        model = model.Model(args, checkpoint)
+        t = VideoTester(args, model, checkpoint)
+        t.test()
+    else:
+        if checkpoint.ok:
+            print("args:", args)
+            loader = data.Data(args)
+            _model = model.Model(args, checkpoint)
+            _loss = loss.Loss(args, checkpoint) if not args.test_only else None
+            t = Trainer(args, loader, _model, _loss, checkpoint)
+            while not t.terminate():
+                t.train()
+                t.test()
+
+            checkpoint.done()
+
+if __name__ == '__main__':
+    main()
