25d24108564dea71f6ba580fccb073903012b5f1
JasonPark <67350632+FinalCold@users.noreply.github.com>
2024-01-16 13:50:18 +0900

    feat: co detr config (#54)
    #51

---
+++ mmdetection/custom_config/recycle_co-detr_swin-l_5scale_lsj.py
@@ -0,0 +1,474 @@
+import sys
+
+sys.path.insert(0, "../mmdetection/projects")
+
+_base_ = [
+    '../configs/_base_/datasets/coco_detection.py',
+    '../configs/_base_/schedules/schedule_1x.py',
+    '../configs/_base_/default_runtime.py'
+]
+
+custom_imports = dict(
+    imports=['projects.CO-DETR.codetr'], allow_failed_imports=False)
+
+pretrained = 'https://github.com/SwinTransformer/storage/releases/download/v1.0.0/swin_large_patch4_window12_384_22k.pth'  # noqa
+
+### Model ###
+### 중요 !!! -> num_classes 개수 잘 확인 !
+num_dec_layer = 6
+loss_lambda = 2.0
+num_classes = 10
+
+model = dict(
+    type='CoDETR',
+    # If using the lsj augmentation,
+    # it is recommended to set it to True.
+    use_lsj=True,
+    # detr: 52.1
+    # one-stage: 49.4
+    # two-stage: 47.9
+    eval_module='detr',  # in ['detr', 'one-stage', 'two-stage']
+    data_preprocessor=dict(
+        type='DetDataPreprocessor',
+        mean=[123.675, 116.28, 103.53],
+        std=[58.395, 57.12, 57.375],
+        bgr_to_rgb=True),
+    backbone=dict(
+        type='SwinTransformer',
+        pretrain_img_size=384,
+        embed_dims=192,
+        depths=[2, 2, 18, 2],
+        num_heads=[6, 12, 24, 48],
+        window_size=12,
+        mlp_ratio=4,
+        qkv_bias=True,
+        qk_scale=None,
+        drop_rate=0.,
+        attn_drop_rate=0.,
+        drop_path_rate=0.3,
+        patch_norm=True,
+        out_indices=(0, 1, 2, 3),
+        # Please only add indices that would be used
+        # in FPN, otherwise some parameter will not be used
+        with_cp=False,
+        convert_weights=True,
+        init_cfg=dict(type='Pretrained', checkpoint=pretrained)),
+    neck=dict(
+        type='ChannelMapper',
+        in_channels=[192, 384, 768, 1536],
+        kernel_size=1,
+        out_channels=256,
+        act_cfg=None,
+        norm_cfg=dict(type='GN', num_groups=32),
+        num_outs=5),
+    query_head=dict(
+        type='CoDINOHead',
+        num_query=900,
+        num_classes=num_classes,
+        in_channels=2048,
+        as_two_stage=True,
+        dn_cfg=dict(
+            label_noise_scale=0.5,
+            box_noise_scale=1.0,
+            group_cfg=dict(dynamic=True, num_groups=None, num_dn_queries=100)),
+        transformer=dict(
+            type='CoDinoTransformer',
+            with_coord_feat=False,
+            num_co_heads=2,  # ATSS Aux Head + Faster RCNN Aux Head
+            num_feature_levels=5,
+            encoder=dict(
+                type='DetrTransformerEncoder',
+                num_layers=6,
+                # number of layers that use checkpoint.
+                # The maximum value for the setting is num_layers.
+                # FairScale must be installed for it to work.
+                with_cp=6,
+                transformerlayers=dict(
+                    type='BaseTransformerLayer',
+                    attn_cfgs=dict(
+                        type='MultiScaleDeformableAttention',
+                        embed_dims=256,
+                        num_levels=5,
+                        dropout=0.0),
+                    feedforward_channels=2048,
+                    ffn_dropout=0.0,
+                    operation_order=('self_attn', 'norm', 'ffn', 'norm'))),
+            decoder=dict(
+                type='DinoTransformerDecoder',
+                num_layers=6,
+                return_intermediate=True,
+                transformerlayers=dict(
+                    type='DetrTransformerDecoderLayer',
+                    attn_cfgs=[
+                        dict(
+                            type='MultiheadAttention',
+                            embed_dims=256,
+                            num_heads=8,
+                            dropout=0.0),
+                        dict(
+                            type='MultiScaleDeformableAttention',
+                            embed_dims=256,
+                            num_levels=5,
+                            dropout=0.0),
+                    ],
+                    feedforward_channels=2048,
+                    ffn_dropout=0.0,
+                    operation_order=('self_attn', 'norm', 'cross_attn', 'norm',
+                                     'ffn', 'norm')))),
+        positional_encoding=dict(
+            type='SinePositionalEncoding',
+            num_feats=128,
+            temperature=20,
+            normalize=True),
+        loss_cls=dict(  # Different from the DINO
+            type='QualityFocalLoss',
+            use_sigmoid=True,
+            beta=2.0,
+            loss_weight=1.0),
+        loss_bbox=dict(type='L1Loss', loss_weight=5.0),
+        loss_iou=dict(type='GIoULoss', loss_weight=2.0)),
+
+    rpn_head=dict(
+        type='RPNHead',
+        in_channels=256,
+        feat_channels=256,
+        anchor_generator=dict(
+            type='AnchorGenerator',
+            octave_base_scale=4,
+            scales_per_octave=3,
+            ratios=[0.5, 1.0, 2.0],
+            strides=[4, 8, 16, 32, 64, 128]),
+        bbox_coder=dict(
+            type='DeltaXYWHBBoxCoder',
+            target_means=[.0, .0, .0, .0],
+            target_stds=[1.0, 1.0, 1.0, 1.0]),
+        loss_cls=dict(
+            type='CrossEntropyLoss',
+            use_sigmoid=True,
+            loss_weight=1.0 * num_dec_layer * loss_lambda),
+        loss_bbox=dict(
+            type='L1Loss', loss_weight=1.0 * num_dec_layer * loss_lambda)
+        ),
+    roi_head=[
+        dict(
+            type='CoStandardRoIHead',
+            bbox_roi_extractor=dict(
+                type='SingleRoIExtractor',
+                roi_layer=dict(
+                    type='RoIAlign', output_size=7, sampling_ratio=0),
+                out_channels=256,
+                featmap_strides=[4, 8, 16, 32, 64],
+                finest_scale=56),
+            bbox_head=dict(
+                type='Shared2FCBBoxHead',
+                in_channels=256,
+                fc_out_channels=1024,
+                roi_feat_size=7,
+                num_classes=num_classes,
+                bbox_coder=dict(
+                    type='DeltaXYWHBBoxCoder',
+                    target_means=[0., 0., 0., 0.],
+                    target_stds=[0.1, 0.1, 0.2, 0.2]),
+                reg_class_agnostic=False,
+                reg_decoded_bbox=True,
+                loss_cls=dict(
+                    type='CrossEntropyLoss',
+                    use_sigmoid=False,
+                    loss_weight=1.0 * num_dec_layer * loss_lambda),
+                loss_bbox=dict(
+                    type='GIoULoss',
+                    loss_weight=10.0 * num_dec_layer * loss_lambda)))
+    ],
+    bbox_head=[
+        dict(
+            type='CoATSSHead',
+            num_classes=num_classes,
+            in_channels=256,
+            stacked_convs=1,
+            feat_channels=256,
+            anchor_generator=dict(
+                type='AnchorGenerator',
+                ratios=[1.0],
+                octave_base_scale=8,
+                scales_per_octave=1,
+                strides=[4, 8, 16, 32, 64, 128]),
+            bbox_coder=dict(
+                type='DeltaXYWHBBoxCoder',
+                target_means=[.0, .0, .0, .0],
+                target_stds=[0.1, 0.1, 0.2, 0.2]),
+            loss_cls=dict(
+                type='FocalLoss',
+                use_sigmoid=True,
+                gamma=2.0,
+                alpha=0.25,
+                loss_weight=1.0 * num_dec_layer * loss_lambda),
+            loss_bbox=dict(
+                type='GIoULoss',
+                loss_weight=2.0 * num_dec_layer * loss_lambda),
+            loss_centerness=dict(
+                type='CrossEntropyLoss',
+                use_sigmoid=True,
+                loss_weight=1.0 * num_dec_layer * loss_lambda)),
+    ],
+    # model training and testing settings
+    train_cfg=[
+        dict(
+            assigner=dict(
+                type='HungarianAssigner',
+                match_costs=[
+                    dict(type='FocalLossCost', weight=2.0),
+                    dict(type='BBoxL1Cost', weight=5.0, box_format='xywh'),
+                    dict(type='IoUCost', iou_mode='giou', weight=2.0)
+                ])),
+        dict(
+            rpn=dict(
+                assigner=dict(
+                    type='MaxIoUAssigner',
+                    pos_iou_thr=0.7,
+                    neg_iou_thr=0.3,
+                    min_pos_iou=0.3,
+                    match_low_quality=True,
+                    ignore_iof_thr=-1),
+                sampler=dict(
+                    type='RandomSampler',
+                    num=256,
+                    pos_fraction=0.5,
+                    neg_pos_ub=-1,
+                    add_gt_as_proposals=False),
+                allowed_border=-1,
+                pos_weight=-1,
+                debug=False),
+            rpn_proposal=dict(
+                nms_pre=4000,
+                max_per_img=1000,
+                nms=dict(type='nms', iou_threshold=0.7),
+                min_bbox_size=0),
+            rcnn=dict(
+                assigner=dict(
+                    type='MaxIoUAssigner',
+                    pos_iou_thr=0.5,
+                    neg_iou_thr=0.5,
+                    min_pos_iou=0.5,
+                    match_low_quality=False,
+                    ignore_iof_thr=-1),
+                sampler=dict(
+                    type='RandomSampler',
+                    num=512,
+                    pos_fraction=0.25,
+                    neg_pos_ub=-1,
+                    add_gt_as_proposals=True),
+                pos_weight=-1,
+                debug=False)
+            ),
+        dict(
+            assigner=dict(type='ATSSAssigner', topk=9),
+            allowed_border=-1,
+            pos_weight=-1,
+            debug=False)
+    ],
+    test_cfg=[
+        # Deferent from the DINO, we use the NMS.
+        dict(
+            max_per_img=300,
+            # NMS can improve the mAP by 0.2.
+            nms=dict(type='soft_nms', iou_threshold=0.8)),
+        dict(
+            rpn=dict(
+                nms_pre=1000,
+                max_per_img=1000,
+                nms=dict(type='nms', iou_threshold=0.7),
+                min_bbox_size=0),
+            rcnn=dict(
+                score_thr=0.0,
+                nms=dict(type='nms', iou_threshold=0.5),
+                max_per_img=100)
+                ),
+        dict(
+            # atss bbox head:
+            nms_pre=1000,
+            min_bbox_size=0,
+            score_thr=0.0,
+            nms=dict(type='nms', iou_threshold=0.6),
+            max_per_img=100),
+        # soft-nms is also supported for rcnn testing
+        # e.g., nms=dict(type='soft_nms', iou_threshold=0.5, min_score=0.05)
+    ])
+
+### Dataset ###
+### 중요 ! -> 데이터셋 경로 및 json 파일 확인 !!! ###
+data_root = '/data/ephemeral/home/dataset/'
+metainfo = {
+    'classes': ('General trash', 'Paper', 'Paper pack', 'Metal', 'Glass',
+                'Plastic', 'Styrofoam', 'Plastic bag', 'Battery', 'Clothing',),
+    'palette': [
+        (220, 20, 60), (119, 11, 32), (0, 0, 230), (106, 0, 228), (60, 20, 220),
+        (0, 80, 100), (0, 0, 70), (50, 0, 192), (250, 170, 30), (255, 0, 0)
+    ]
+}
+color_space = [
+    [dict(type='ColorTransform')],
+    [dict(type='AutoContrast')],
+    [dict(type='Equalize')],
+    [dict(type='Sharpness')],
+    [dict(type='Posterize')],
+    [dict(type='Solarize')],
+    [dict(type='Color')],
+    [dict(type='Contrast')],
+    [dict(type='Brightness')],
+]
+
+### Image Size 확인 !!! ###
+image_size = (1024, 1024)
+
+### Augmentation Rule 확인 !!! ###
+train_pipeline = [
+    dict(type='LoadImageFromFile', backend_args=None),
+    dict(type='LoadAnnotations', with_bbox=True),
+    dict(type='Resize', scale=image_size, keep_ratio=True),
+    dict(type='RandomFlip', prob=0.5),
+    dict( # LSJ
+        type='RandomResize',
+        scale=image_size,
+        ratio_range=(0.1, 2.0),
+        keep_ratio=True),
+    dict(
+        type='RandomCrop',
+        crop_type='absolute_range',
+        crop_size=image_size,
+        recompute_bbox=True,
+        allow_negative_crop=True),
+    # dict(type='RandAugment', aug_space=color_space, aug_num=1),
+    dict(type='PackDetInputs')
+]
+
+train_dataloader = dict(
+    batch_size=1,
+    num_workers=4,
+    sampler=dict(type='DefaultSampler', shuffle=True),
+    dataset=dict(
+        data_root=data_root,
+        metainfo=metainfo,
+        ### json 경로 확인 !!! ###
+        ann_file='train_kfold_0.json',
+        data_prefix=dict(img=''),
+        pipeline=train_pipeline))
+
+test_pipeline = [
+    dict(type='LoadImageFromFile', backend_args=None),
+    dict(type='Resize', scale=image_size, keep_ratio=True),
+    dict(type='LoadAnnotations', with_bbox=True),
+    dict(type='PackDetInputs', meta_keys=('img_id', 'img_path', 'ori_shape', 'img_shape', 'scale_factor'))
+]
+
+val_dataloader = dict(
+    batch_size=1,
+    num_workers=4,
+    dataset=dict(
+        data_root=data_root,
+        metainfo=metainfo,
+        ### json 경로 확인 !!! ###
+        ann_file='val_kfold_0.json',
+        data_prefix=dict(img=''),
+        pipeline=test_pipeline))
+
+test_dataloader = dict(
+    batch_size=1,
+    num_workers=4,
+    dataset=dict(
+        data_root=data_root,
+        metainfo=metainfo,
+        ### json 경로 확인 !!! ###
+        ann_file='test.json',
+        data_prefix=dict(img=''),
+        pipeline=test_pipeline))
+
+val_evaluator = dict(
+    type='CocoMetric',
+    ### json 경로 확인 !!! ###
+    ann_file=data_root + 'val_kfold_0.json',
+    metric='bbox',
+    format_only=False,
+    classwise=True,
+    )
+
+test_evaluator = dict(
+    type='CocoMetric',
+    ### json 경로 확인 !!! ###
+    ann_file=data_root + 'test.json',
+    metric='bbox',
+    format_only=False,
+    classwise=True,
+    )
+
+
+### Optimizer ###
+optim_wrapper = dict(
+    _delete_=True,
+    type='OptimWrapper',
+    optimizer=dict(type='AdamW', lr=2e-4, weight_decay=0.0001),
+    clip_grad=dict(max_norm=0.1, norm_type=2),
+    paramwise_cfg=dict(custom_keys={'backbone': dict(lr_mult=0.1)}))
+
+
+
+### Loader Config ###
+max_epochs = 12
+train_cfg = dict(
+    _delete_=True,
+    type='EpochBasedTrainLoop',
+    max_epochs=max_epochs,
+    val_interval=1)
+val_cfg = dict(type='ValLoop')
+test_cfg = dict(type='TestLoop')
+
+### Scheduler ###
+param_scheduler = [
+    # dict(
+    #     type='MultiStepLR',
+    #     begin=0,
+    #     end=max_epochs,
+    #     by_epoch=True,
+    #     milestones=[7, 11],
+    #     gamma=0.1)
+    dict(
+        type='CosineAnnealingLR',
+        eta_min=0.0,
+        begin=0,
+        T_max=max_epochs,
+        end=max_epochs,
+        by_epoch=True,
+        convert_to_iter_based=True)
+]
+
+#### Hook ####
+# Test시 inference 결과를 이미지로 확인하고 싶으면 아래 주석 해제
+# default_hooks = dict(visualization=dict(type="DetVisualizationHook",draw=True))
+default_hooks = dict(checkpoint=dict(by_epoch=True, interval=1, max_keep_ckpts=3))
+custom_hooks = [dict(type='SubmissionHook')]
+log_processor = dict(by_epoch=True)
+
+### Wandb ###
+### name 잘 확인 !!! ###
+vis_backends = [
+    dict(type='LocalVisBackend'),
+    dict(type='WandbVisBackend',
+         init_kwargs={
+            'project': 'mmdetection',
+            'entity': 'ai_tech_level2_objectdetection2',
+            'group': 'co_detr',
+            'name': 'swin-l_original_epochs12'  # name 변경 해줄 것!
+         })]
+
+visualizer = dict(
+    type='DetLocalVisualizer',
+    vis_backends=vis_backends,
+    name='visualizer')
+
+
+# NOTE: `auto_scale_lr` is for automatically scaling LR,
+# USER SHOULD NOT CHANGE ITS VALUES.
+# base_batch_size = (8 GPUs) x (2 samples per GPU)
+### Multi GPU 일시, base_batch_size 변경 !!!
+auto_scale_lr = dict(base_batch_size=2)
+
+load_from = 'https://download.openmmlab.com/mmdetection/v3.0/codetr/co_dino_5scale_lsj_swin_large_1x_coco-3af73af2.pth'
